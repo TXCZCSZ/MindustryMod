60d248d45fb0e35c53a3fb67ac622c72f51d9d09
TXCZCSZ <2786712140@qq.com>
2024-04-06 14:55:24 +0800

    hh

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -18,11 +18,11 @@ public class ExampleJavaMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                BaseDialog dialog = new BaseDialog("TXCZCSZ");
+                dialog.cont.add("我的第一个模组").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.button("好哎⸜₍๑•⌔•๑₎⸝", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
