9cf79bba51d559799fd6c55efffd97d009608244
TXCZCSZ <2786712140@qq.com>
2024-04-07 11:03:14 +0800

    。

--- src/example/content/Blocks.java
+++ src/example/content/Blocks.java
@@ -3,6 +3,7 @@ package example.content;
 import example.world.blocks.power.*;
 import mindustry.content.Items;
 import mindustry.world.Block;
+import mindustry.world.meta.*;
 import mindustry.world.blocks.power.PowerNode;
 public class Blocks {
     public static Block 
