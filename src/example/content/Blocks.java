beca35b2efff9b2843a3db42788eaa6ce42b5b9f
TXCZCSZ <2786712140@qq.com>
2024-04-07 11:11:44 +0800

    hahahahjs

--- src/example/content/Blocks.java
+++ src/example/content/Blocks.java
@@ -7,9 +7,9 @@ import mindustry.world.meta.*;
 import mindustry.world.blocks.power.PowerNode;
 public class Blocks {
     public static Block 
-    YingjiFadianjj;
+    yingjiFadianjj;
     public static void load(){
-        YingjiFadianjj = new YingjiFadianjj("应急发电机"){{
+        yingjiFadianjj = new YingjiFadianjj("应急发电机"){{
             requirements(Category.power, with(Items.copper, 1, Items.lead, 3));
             maxNodes = 10;
             laserRange = 6;
